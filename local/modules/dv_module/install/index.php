08b694bd50d7cd6f16c09be9fdffb0ec678af013
полина <PDBoeva@stud.kantiana.ru>
2024-04-15 21:52:29 +0200

    3 задание

---
+++ local/modules/dv_module/install/index.php
@@ -0,0 +1,50 @@
+<?
+Class dv_module extends CModule
+{
+var $MODULE_ID = "dv_module";
+var $MODULE_VERSION;
+var $MODULE_VERSION_DATE;
+var $MODULE_NAME;
+var $MODULE_DESCRIPTION;
+var $MODULE_CSS;
+function __construct()
+{
+$arModuleVersion = array();
+$path = str_replace("\\", "/", __FILE__);
+$path = substr($path, 0, strlen($path) - strlen("/index.php"));
+include($path."/version.php");
+if (is_array($arModuleVersion) && array_key_exists("VERSION", $arModuleVersion))
+{
+$this->MODULE_VERSION = $arModuleVersion["VERSION"];
+$this->MODULE_VERSION_DATE = $arModuleVersion["VERSION_DATE"];
+}
+$this->MODULE_NAME = "Модуль с компонентами";
+$this->MODULE_DESCRIPTION = "Модуль с компонентами";
+}
+function InstallFiles()
+{
+CopyDirFiles($_SERVER["DOCUMENT_ROOT"]."/local/modules/dv_module/install/components",
+             $_SERVER["DOCUMENT_ROOT"]."/bitrix/components", true, true);
+return true;
+}
+function UnInstallFiles()
+{
+DeleteDirFilesEx("/local/components/dv");
+return true;
+}
+function DoInstall()
+{
+global $DOCUMENT_ROOT, $APPLICATION;
+$this->InstallFiles();
+RegisterModule("dv_module");
+$APPLICATION->IncludeAdminFile("Установка модуля dv_module", $DOCUMENT_ROOT."/local/modules/dv_module/install/step.php");
+}
+function DoUninstall()
+{
+global $DOCUMENT_ROOT, $APPLICATION;
+$this->UnInstallFiles();
+UnRegisterModule("dv_module");
+$APPLICATION->IncludeAdminFile("Деинсталляция модуля dv_module", $DOCUMENT_ROOT."/local/modules/dv_module/install/unstep.php");
+}
+}
+?>
---
+++ local/modules/dv_module/install/version.php
@@ -0,0 +1,5 @@
+<?
+$arModuleVersion = array(
+"VERSION" => "1.0.0",
+"VERSION_DATE" => "2024-04-15 17:00:00");
+?>
